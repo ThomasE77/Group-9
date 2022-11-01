21caf920692fc7f3400f1cb6f198a7bf66502e4d
ThomasE77 <80560590+ThomasE77@users.noreply.github.com>
2022-11-01 17:17:10 -0500

    reset test.py

--- core/tests.py
+++ core/tests.py
@@ -1,11 +1,1 @@
-from django.test import TestCase
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
-from django.contrib import messages
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-from .models import Profile, Post, LikePost, FollowersCount
-from itertools import chain
-import random
-
 # Create your tests here.
